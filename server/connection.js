c867ee849626b53bc2140ab6dcc6a0b3021d7fe6
kaif <mohdkaif30062002@gamil.com>
2023-08-17 17:54:25 +0530

    updated

--- server/connection.js
+++ server/connection.js
@@ -3,7 +3,10 @@ const mongoose =require("mongoose");
 const dotenv =require("dotenv");
 dotenv.config();
 const CONNECTION=process.env.MONGODB_CONNECTION;
-mongoose.connect(CONNECTION).then(()=>{
+mongoose.connect(CONNECTION,{
+    useUnifiedTopology:true,
+    useNewUrlParser:true
+}).then(()=>{
     console.log("connected");
 }).catch((err)=>{
     console.log(`{err} Did not connected`);
